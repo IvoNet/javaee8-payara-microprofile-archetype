399ebbe6187b38275a2a3698a08d6bb271b6af05
Ivo Woltring <Ivo@IvoNet.nl>
2019-02-04 08:21:02 +0100

    if no image is there no error is shown

--- src/main/resources/archetype-resources/buildAndRun.sh
+++ src/main/resources/archetype-resources/buildAndRun.sh
@@ -3,4 +3,4 @@
 #set( $symbol_escape = '\' )
 #!/usr/bin/env bash
 mvn clean package && docker build -t ${docker-hub-name}/${artifactId} .
-docker rm -f ${artifactId} || true && docker run -it --name ${artifactId} -p 8080:8080 -p 4848:4848 -p 8181:8181 --name ${artifactId} ${docker-hub-name}/${artifactId}
+docker rm -f ${artifactId} 2>/dev/null || true && docker run -it --name ${artifactId} -p 8080:8080 -p 4848:4848 -p 8181:8181 --name ${artifactId} ${docker-hub-name}/${artifactId}
